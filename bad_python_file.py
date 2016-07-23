9c926a8dbf5be7a9fcf0853eb8d11eef53fe5f20
Janos Brezniczky <brezniczky@gmail.com>
2016-07-23 15:23:32 +0100

    Issue #1: breaking it again + autopep8 in travis

--- bad_python_file.py
+++ bad_python_file.py
@@ -1,15 +1,13 @@
-import verylongmodulename
-
+ import verylongmodulename
 
 class class1:
     def __init__():
         pass
 
     class class2:
-        verylongmodulename.verylongfunctionname(
-            verylongvariable1, [verylongvariable2, verylongvariable3])
+        verylongmodulename.verylongfunctionname(verylongvariable1, [verylongvariable2, verylongvariable3]) 
 
 
-if __name__ == "__main__":
-    print("hello " +
+if __name__ == "__main__": 
+    print("hello " + \
           "word")
