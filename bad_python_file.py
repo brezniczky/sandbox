191f6403255701996ea05d8285e923dbea88d0fd
Janos Brezniczky <brezniczky@gmail.com>
2016-07-23 01:39:35 +0100

    ill-formatted file vs travis.yml, round 1

---
+++ bad_python_file.py
@@ -0,0 +1,16 @@
+ # this is
+#a very
+
+import verylongmodulename
+
+class class1: 
+  def __init__(): 
+    pass
+
+  class class2: 
+    verylongmodulename.verylongfunctionname(verylongvariable1, [verylongvariable2, verylongvariable3])
+
+
+if __name__ == "__main__":
+  print("hello " + \
+        "word" )
